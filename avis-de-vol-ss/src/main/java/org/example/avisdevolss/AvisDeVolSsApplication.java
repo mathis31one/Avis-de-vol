9690e8c8e26e02573230a4d6da8075bad8aa55bb
mathis31one <mathis.oneda31@gmail.com>
2025-06-30 09:35:26 +0200

    init

---
+++ avis-de-vol-ss/src/main/java/org/example/avisdevolss/AvisDeVolSsApplication.java
@@ -0,0 +1,13 @@
+package org.example.avisdevolss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisDeVolSsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisDeVolSsApplication.class, args);
+	}
+
+}
